342ccca088b666e38d3178a2c1cf658d1f553c31
Saurav Thoke <sauravthoke28@gmail.com>
2024-10-05 18:54:37 +0530

    First commit project begins

---
+++ ArtCorner/src/main/java/com/saurav/ArtCorner/ArtCornerApplication.java
@@ -0,0 +1,13 @@
+package com.saurav.ArtCorner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtCornerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtCornerApplication.class, args);
+	}
+
+}
---
+++ ArtCorner/src/main/java/com/saurav/ArtCorner/controller/HomeController.java
@@ -0,0 +1,4 @@
+package com.saurav.ArtCorner.controller;
+
+public class HomeController {
+}
